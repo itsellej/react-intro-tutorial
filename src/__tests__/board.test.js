a79b7b96fd29479468ba8623b3fdafb502afbee0
Dan Pelensky <dan@pelensky.com>
2019-02-01 11:47:05 +0000

    create board test

---
+++ src/__tests__/board.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Board } from '../board';
+
+it('renders without crashing', () => {
+  shallow(<Board squares={[0, 1, 2, 3,4,5,6,7,8]} />);
+});
+
+it('renders squares', () => {
+  const wrapper = shallow(<Board squares={[0, 1, 2, 3,4,5,6,7,8]} />);
+  console.log("wrapper", wrapper.debug())
+});
